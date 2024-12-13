20c7c986013fb2d02a775659d97b3897ae77fe1b
Lakshmi Prasanna <prasannapenmetsa763@gmail.com>
2024-12-13 22:11:11 +0530

    Uploading code: Biggest of Two Numbers.c

---
+++ Biggest of Two Numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    int X, Y;
+    scanf("%d", &X);
+    scanf("%d", &Y);
+    if (X > Y) 
+    {
+        printf("%d\n", X);
+    } 
+    else 
+    {
+        printf("%d\n", Y);
+    }
+}
