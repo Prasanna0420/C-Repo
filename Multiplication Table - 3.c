50374de8db1d21134d3c82792aec92b4ba987cf6
Lakshmi Prasanna <prasannapenmetsa763@gmail.com>
2024-12-14 00:42:54 +0530

    Uploading code: Multiplication Table - 3.c

---
+++ Multiplication Table - 3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int N, A, B;
+    scanf("%d %d %d", &N, &A, &B);
+    for (int i = A; i <= B; i++) 
+    {
+        printf("%d x %d = %d\n", N, i, N * i);
+    }
+}
